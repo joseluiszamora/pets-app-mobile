1d030f3eb6af84396d475d069131a71ea3342471
jzamora <jzamora@miteleferico.bo>
2024-10-22 20:59:16 -0400

    add google login

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pets_app_mobile/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/blocs/blocs.dart';
 import 'core/routes/app_router.dart';
 import 'core/themes/app_themes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Supabase.initialize(
+    url: 'https://yodukrtarbjndxabgmii.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvZHVrcnRhcmJqbmR4YWJnbWlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU2NDg5MzksImV4cCI6MjA0MTIyNDkzOX0.Nu7R5Dq0WrDY4W5v2p-rq1G5o2sPrOywgpUKiAiluXE',
+  );
   serviceLocatorInit();
   runApp(const BlocProviders());
 }
--- lib/views/auth/login_page.dart
+++ lib/views/auth/login_page.dart
@@ -1,10 +1,91 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:math';
 
-class LoginPage extends StatelessWidget {
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String? _userId;
+  final supabase = Supabase.instance.client;
+
+  @override
+  void initState() {
+    super.initState();
+
+    supabase.auth.onAuthStateChange.listen((data) {
+      setState(() {
+        _userId = data.session?.user.id;
+      });
+    });
+  }
+
+  Future<void> _nativeGoogleSignIn() async {
+    /// Web Client ID that you registered with Google Cloud.
+    const webClientId =
+        '68837437433-qd5n2abfr8r9mc6onopl6oqcodka750u.apps.googleusercontent.com';
+
+    /// TODO: update the iOS client ID with your own.
+    ///
+    /// iOS Client ID that you registered with Google Cloud.
+    const iosClientId =
+        '68837437433-di5l5nafvclclv25gn07l5f6sicv8u6g.apps.googleusercontent.com';
+
+    // Google sign in on Android will work without providing the Android
+    // Client ID registered on Google Cloud.
+
+    final GoogleSignIn googleSignIn = GoogleSignIn(
+      clientId: iosClientId,
+      serverClientId: webClientId,
+    );
+    final googleUser = await googleSignIn.signIn();
+    final googleAuth = await googleUser!.authentication;
+    final accessToken = googleAuth.accessToken;
+    final idToken = googleAuth.idToken;
+
+    if (accessToken == null) {
+      throw 'No Access Token found.';
+    }
+    if (idToken == null) {
+      throw 'No ID Token found.';
+    }
+
+    await supabase.auth.signInWithIdToken(
+      provider: OAuthProvider.google,
+      idToken: idToken,
+      accessToken: accessToken,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Text("Login Page");
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_userId ?? 'Not logged in'),
+            ElevatedButton(
+              onPressed: () async {
+                if (!kIsWeb && (Platform.isAndroid || Platform.isIOS)) {
+                  await _nativeGoogleSignIn();
+                  return;
+                }
+                await supabase.auth.signInWithOAuth(OAuthProvider.google);
+              },
+              child: const Text('Login with Google'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
